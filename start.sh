a59530605dc13d928b8a94aee20d0fa5dec47150
stevan06v <stevanvlajic5@gmail.com>
2024-01-16 09:33:29 +0100

    feat: added start-script for seamless startup functionality close #22

--- start.sh
+++ start.sh
@@ -1,6 +1,17 @@
-npm i && npm run build 
-
+# install composer dependencies
 composer install
 
-php artisan migrate:fresh --seed
+# rename .env.example .env
+mv .env.example .env
 
+# install node dependencies
+npm install
+
+# generate application key
+php artisan key:generate
+
+# drop & create tables
+php artisan migrate:fresh
+
+# start vite dev-server
+npm run dev
