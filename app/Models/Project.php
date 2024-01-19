494337c19bb55b1b767d73fb198733855b12a5cf
stevan06v <stevanvlajic5@gmail.com>
2024-01-19 08:56:40 +0100

    feat: added ide-helper dependency & created php-docs for models for eloquent ide support & created commands file for docs #15

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,7 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Builder;
 
+/**
+ * Post
+ *
+ * @mixin Builder
+ * @property int $id
+ * @property int $user_id
+ * @property string $project_url
+ * @property string $project_identification
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Project newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Project newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Project query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereProjectIdentification($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereProjectUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Project extends Model
 {
     use HasFactory;
