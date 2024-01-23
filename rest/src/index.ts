ddc52bb8bf727cacf821a6ccad674d588b6e0c26
Jonas <publik.froeller.jonas@gmx.at>
2024-01-23 09:58:01 +0100

    rest(/organization/:organization_name): first graphql endpoint (closes #19)

--- rest/src/index.ts
+++ rest/src/index.ts
@@ -1,7 +1,101 @@
-import { Elysia } from "elysia";
+import { Elysia } from "elysia"; // https://elysiajs.com/introduction.html
+import { Octokit } from "octokit"; // unused: App // https://github.com/octokit/octokit.js | https://github.com/octokit/types.ts
+import 'dotenv/config'; // process.env.<ENV_VAR_NAME>
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const GITHUB_PAT = process.env.GITHUB_PAT;
+const octokit = new Octokit({ auth: GITHUB_PAT });
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get('/organization/:organization_name', async ({ params: { organization_name } }) => {
+  const {
+    organization: organization,
+  } = await octokit.graphql(`{
+    organization(login: ${"\"" + organization_name + "\""}) {
+      name
+      description
+      url
+      teams(first: 10) {
+        totalCount
+        nodes {
+          name
+        }
+      }
+      membersWithRole(first: 10) {
+        totalCount
+        nodes {
+          name
+        }
+      }
+      repositories(first: 10) {
+        nodes {
+          name
+        }
+      }
+      projectsV2(first: 10) {
+        totalCount
+        nodes {
+          title
+        }
+      }
+      websiteUrl
+      email
+    }
+  }`);
+
+  return JSON.stringify(organization, null, 2);
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+/* Testing query (test on https://docs.github.com/en/graphql/overview/explorer)
+query {
+  repository(owner: "propromo-software", name: "propromo") {
+    projectV2(number: 1) {
+      title
+      shortDescription
+      url
+      public
+      readme
+      createdAt
+      updatedAt
+      items(first: 10) {
+        totalCount # total issues
+      }
+      view(number: 1) {
+        name
+        filter
+      }
+      creator {
+        login
+      }
+      repositories(first: 10) {
+        totalCount
+        nodes {
+          name
+          issues(first: 50, states: [OPEN]) {
+            totalCount
+            nodes {
+              title
+            }
+          }
+        }
+      }
+    }
+    projectsV2(first: 10) {
+      totalCount
+      nodes {
+        number
+        title
+        shortDescription
+        views {
+          totalCount
+        }
+      }
+    }
+  }
+}
+*/
