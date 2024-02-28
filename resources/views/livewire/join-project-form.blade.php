ace3f3ae3a1a56e0bd94615270295ae0617b2938
stevan06v <stevanvlajic5@gmail.com>
2024-02-28 23:21:23 +0100

    fix: blocking rerendering js for input field closes #97

--- resources/views/livewire/join-project-form.blade.php
+++ resources/views/livewire/join-project-form.blade.php
@@ -7,16 +7,17 @@
             <sl-input type="text" id="url" value="https://github.com/propromo-software"
                       placeholder="Here goes the project-url"
                       wire:model="projectUrl"
+                      wire:ignore
                       class="w-full"
             >
             </sl-input>
-            <sl-button wire:click="save" wire:loading.attr="disabled">JOIN</sl-button>
+            <sl-button wire:click="save" wire:loading.attr="disabled" wire:ignore>JOIN</sl-button>
         </div>
     </form>
 
 
     @if ($createProjectError)
-        <sl-alert variant="danger" duration="3000" closable>
+        <sl-alert variant="danger" duration="3000" closable wire:ignore>
             <sl-icon slot="icon" name="exclamation-octagon"></sl-icon>
             <strong>Project creation-error</strong><br/>
             {{$createProjectError}}
