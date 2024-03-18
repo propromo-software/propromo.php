c83a260e3815f17adbff08371f89782f0064b929
stevan06v <stevanvlajic5@gmail.com>
2024-03-18 15:16:09 +0100

    fix: added check if user already joined the project (closes #133) #build-for-review

--- app/Traits/ProjectJoiner.php
+++ app/Traits/ProjectJoiner.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace App\Traits;
+
 use App\Models\Project;
+use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,11 +16,22 @@ trait ProjectJoiner
     {
         $project = Project::whereProjectHash($project_hash)->first();
 
-        if(!is_null($project)){
-            $project->users()->attach(Auth::user()->id);
-            return $project;
+        if (!is_null($project)) {
+
+            $current_user_projects = User::find(Auth::user()->id)
+                ->projects()
+                ->where("project_hash", "=", $project_hash)
+                ->get();
+
+            if ($current_user_projects->count() > 0) {
+                throw new Exception("You have already joined the monitor!");
+            } else {
+                $project->users()->attach(Auth::user()->id);
+                return $project;
+            }
+        } else {
+            throw new Exception("No monitor with that ID found!");
         }
-        throw new Exception("No monitor with that ID found!");
     }
 
 }
