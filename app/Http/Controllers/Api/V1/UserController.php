92ed51b8891b7d319362298589259499cdeb1df0
stevan06v <stevanvlajic5@gmail.com>
2024-03-19 19:48:04 +0100

    feat: added post-route for propromo-ios

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\StoreUserRequest;
+use App\Http\Resources\V1\UserCollection;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\V1\UserResource;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        return new UserResource(User::create($request->all()));
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
