d3894e127e38851c0552c599fa19b67b61061f00
stevan06v <stevanvlajic5@gmail.com>
2024-04-07 19:43:43 +0200

    feat: added register route for external services

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreUserRequest;
+use Illuminate\Database\QueryException;
 use App\Http\Resources\V1\UserCollection;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,21 +17,48 @@ class UserController extends Controller
      */
     public function index()
     {
-
+        return new UserCollection(User::all());
     }
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreUserRequest $request)
     {
-        return new UserResource(User::create($request->all()));
+        try {
+            $user = User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "auth_type" => $request->authType,
+                "password" => $request->password
+            ]);
+            if ($user) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'User created successfully',
+                    'data' => $user
+                ], 201);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Failed to create user'
+                ], 500);
+            }
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to create user',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
+
     /**
      * Display the specified resource.
      */
     public function show(User $user)
     {
-
+        return UserResource::make($user);
     }
 
     /**
