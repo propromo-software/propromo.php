e16767b295ddd1857e6ac15cd0d6e1a203058b2c
Jonas <publik.froeller.jonas@gmx.at>
2024-04-23 14:35:42 +0200

    fix(video): closes #184

--- resources/views/livewire/auth/join-monitor.blade.php
+++ resources/views/livewire/auth/join-monitor.blade.php
@@ -30,24 +30,22 @@ new class extends Component {
 }; ?>
 
 
-<div class="mt-4 flex flex-col sm:justify-center items-center sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<div class="flex flex-col items-center mt-4 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
     <div
         class="w-full sm:max-w-md mt-6 p-12 bg-white dark:bg-gray-800 border-[1px] border-border-color overflow-hidden sm:rounded-lg">
 
         <div class="flex justify-center">
             <div class="w-full max-w-md">
-                <h1 class="font-koulen text-6xl text-primary-blue mb-9">JOIN MONITOR</h1>
+                <h1 class="text-6xl font-koulen text-primary-blue mb-9">JOIN MONITOR</h1>
 
                 <form wire:submit="join">
 
                     <sl-input wire:ignore wire:model="monitor_hash" placeholder="Project-Id" type="text"></sl-input>
                     <br>
-                    <iframe class="mt-2" width="100%" height="100"
-                            src="https://youtu.be/SXmJH72-O5g?autoplay=1">
-                    </iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/SXmJH72-O5g?si=if48WPOjUBCZZyi4&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
                     <div class="flex items-center justify-between mt-5">
-                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                        <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                            href="{{ url('create-monitor') }}">
                             No monitor yet?
                         </a>
--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -29,9 +29,7 @@ new class extends Component {
         <livewire:home.join-monitor-form class="mt-20"/>
 
         <h1 class="font-koulen text-4xl text-secondary-grey text-center mt-20">PROJECT PREVIEW</h1>
-        <iframe class="mt-2" width="100%" height="315"
-                src="https://youtu.be/SXmJH72-O5g?autoplay=1">
-        </iframe>
+        <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/SXmJH72-O5g?si=if48WPOjUBCZZyi4&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 </div>
 
