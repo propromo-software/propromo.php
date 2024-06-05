42400e29d9921f064f26bb7f047543634d02c90f
stevan06v <stevanvlajic5@gmail.com>
2024-06-05 11:41:45 +0200

    feat: added styling for tasks-component (currently using dummy data)

--- resources/views/livewire/milestones/show.blade.php
+++ resources/views/livewire/milestones/show.blade.php
@@ -20,11 +20,24 @@ new class extends Component {
 }; ?>
 
 
-<div class="mt-4 mx-8">
+<div>
+    <div class="border-other-grey border-2 rounded-2xl p-8 mt-4 mx-8">
     <div class="flex items-center justify-between mb-5">
-        <a  class="text-secondary-grey text-lg font-sourceSansPro font-bold rounded-md border-2 border-other-grey px-6 py-3"  title="Show Monitor">
-           fsafadsf
-        </a>
+
+        <div class="flex items-center gap-3">
+            <a title="Show Monitor" class="flex items-center">
+                <sl-icon class="cursor-pointer text-primary-blue text-4xl rounded-md border-2 p-2 border-other-grey" name="arrow-left-short" wire:ignore></sl-icon>
+            </a>
+            <a class="flex items-center gap-1 rounded-md border-2 border-other-grey px-6 py-3" wire:click="fireMonitorHashChangedEvent" title="Show User">
+                <sl-icon wire:ignore class="text-secondary-grey font-sourceSansPro text-xl font-bold" name="chat"></sl-icon>
+                <div>
+                    <div class="text-secondary-grey font-sourceSansPro text-lg font-bold">
+                        CONTACT
+                    </div>
+                </div>
+            </a>
+        </div>
+
 
         <div class="flex items-center gap-2">
             <a class="flex items-center gap-1 rounded-md border-2 border-other-grey px-6 py-3" wire:click="fireMonitorHashChangedEvent"  title="Show User">
@@ -43,5 +56,7 @@ new class extends Component {
     <div class="border-other-grey border-2 rounded-2xl p-8 m-2">
         <livewire:tasks.list/>
     </div>
+</div>
+
 </div>
 
