5b7bdaa4a8e60e17faa8ad1459ecee8af35b4be4
stevan06v <stevanvlajic5@gmail.com>
2024-10-01 00:50:41 +0200

    feat: added settings layout & implemented password& email change & change of pat-token for every account

--- resources/views/livewire/settings/navigation.blade.php
+++ resources/views/livewire/settings/navigation.blade.php
@@ -3,16 +3,23 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public $current_section = 'profile';
+
+    public function switch_section($section)
+    {
+        $this->current_section = $section;
+        $this->dispatch('section_changed', $section);
+    }
 }; ?>
 
 <div>
-    <div class="border-other-grey border-2 rounded-md m-4 grid grid-cols-1 justify-center p-3 gap-2.5 text-center w-64">
-        <a href="/settings/profile" class="font-koulen rounded-md text-2xl p-2 {{ request()->is('settings/profile') ? 'bg-primary-blue text-white' : '' }}">
+    <div class="border-other-grey border-2 rounded-md grid grid-cols-1 justify-center p-3 gap-2.5 text-center">
+        <button wire:click="switch_section('profile')" class="{{ $current_section == 'profile' ? 'bg-primary-blue text-white border-primary-blue' : '' }} w-full py-2 border-other-grey border-2 rounded-md font-koulen text-2xl h-min">
             PROFILE
-        </a>
-        <a href="/settings/monitors" class="font-koulen rounded-md text-2xl p-2 {{ request()->is('settings/monitors') ? 'bg-primary-blue text-white' : '' }}">
+        </button>
+        <button wire:click="switch_section('monitors')" class="{{ $current_section == 'monitors' ? 'bg-primary-blue text-white border-primary-blue' : '' }} w-full py-2 border-2 border-other-grey rounded-md font-koulen text-2xl h-min">
             MONITORS
-        </a>
+        </button>
+
     </div>
 </div>
