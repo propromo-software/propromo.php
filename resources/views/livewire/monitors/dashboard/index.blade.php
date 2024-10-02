3c97d965c2ee845197e4d050dc5801376cb88a24
Jonas <publik.froeller.jonas@gmx.at>
2024-10-02 22:55:34 +0200

    improves `Dashboard` styling

--- resources/views/livewire/monitors/dashboard/index.blade.php
+++ resources/views/livewire/monitors/dashboard/index.blade.php
@@ -14,7 +14,7 @@ new class extends Component {
 }; ?>
 
 <div>
-    <div class="p-5 items-center rounded-xl flex gap-10">
+    <div class="flex flex-col items-center gap-5 p-3 sm:p-5 rounded-xl lg:flex-row lg:gap-10">
         <livewire:monitors.dashboard.monitor-dashboard :monitor="$monitor" lazy="true"></livewire:monitors.dashboard.monitor-dashboard>
         <livewire:monitors.dashboard.mini-repository-list :monitor="$monitor" lazy="true"></livewire:monitors.dashboard.mini-repository-list>
         <livewire:monitors.dashboard.top-milestone-list></livewire:monitors.dashboard.top-milestone-list>
--- resources/views/livewire/monitors/dashboard/mini-repository-list.blade.php
+++ resources/views/livewire/monitors/dashboard/mini-repository-list.blade.php
@@ -18,23 +18,20 @@ new class extends Component {
 }; ?>
 
 <div>
-    <h2 class="m-2 font-koulen text-2xl text-primary-blue">Repositories</h2>
-    <div class="grid grid-cols-2 mb-5">
-        <div class="m-2 overflow-auto h-40 border border-other-grey rounded-md p-2.5">
-            <div class="grid grid-cols-2 gap-2">
-                @foreach($monitor->repositories as $repository)
-                    @php
-                        $maxLength = 12;
-                        $shortName = \Illuminate\Support\Str::limit($repository->name, $maxLength, '...');
-                    @endphp
+    <h2 class="m-2 text-2xl font-koulen text-primary-blue">Repositories</h2>
+    <div class="m-2 overflow-auto h-40 border border-other-grey rounded-md p-2.5">
+        <div class="grid grid-cols-1 gap-2 xs:grid-cols-2">
+            @foreach($monitor->repositories as $repository)
+                @php
+                    $maxLength = 12;
+                    $shortName = \Illuminate\Support\Str::limit($repository->name, $maxLength, '...');
+                @endphp
 
-                    <div wire:click="emit_repository_change({{$repository->id}})" wire:key="{{$repository->id}}" class="cursor-pointer flex border-other-grey border-2 rounded-md p-2.5 justify-start gap-2 items-center mb-2">
-                        <sl-icon wire:ignore class="text-2xl text-primary-blue" name="code-square"></sl-icon>
-                        <h1 class="text-primary-blue font-koulen text-xl">{{$shortName}}</h1>
-                    </div>
-                @endforeach
-            </div>
+                <div wire:click="emit_repository_change({{$repository->id}})" wire:key="{{$repository->id}}" class="cursor-pointer flex border-other-grey border-2 rounded-md p-2.5 justify-start gap-2 items-center mb-2">
+                    <sl-icon wire:ignore class="text-2xl text-primary-blue" name="code-square"></sl-icon>
+                    <h1 class="text-xl text-primary-blue font-koulen">{{$shortName}}</h1>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
-
